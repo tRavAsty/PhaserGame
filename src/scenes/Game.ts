a1c5a8199ca50e2329b3168b1b6cbd6f9bf6c95c
Chuyi Liu <lchuyibiu@gmail.com>
2023-04-19 10:37:39 -0400

    init phaser

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -1,24 +1,84 @@
 import Phaser from 'phaser';
 
 export default class Demo extends Phaser.Scene {
+  cursors: Phaser.Types.Input.Keyboard.CursorKeys;
+  player: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody;
   constructor() {
     super('GameScene');
   }
 
   preload() {
     this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('ground', 'assets/platform.png');
+    this.load.image('star', 'assets/star.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.spritesheet('dude',
+        'assets/dude.png',
+        { frameWidth: 32, frameHeight: 48 }
+    );
   }
 
   create() {
-    const logo = this.add.image(400, 70, 'logo');
+    this.add.image(400, 300, 'sky');
+    const platforms = this.physics.add.staticGroup();
 
-    this.tweens.add({
-      targets: logo,
-      y: 350,
-      duration: 1500,
-      ease: 'Sine.inOut',
-      yoyo: true,
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
+
+    this.player = this.physics.add.sprite(100, 450, 'dude');
+    this.player.setBounce(0.2);
+    this.player.setCollideWorldBounds(true);
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.physics.add.collider(this.player, platforms);
+
+    this.anims.create({
+      key: 'left',
+      frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
+      frameRate: 10,
       repeat: -1
     });
+
+    this.anims.create({
+      key: 'turn',
+      frames: [ { key: 'dude', frame: 4 } ],
+      frameRate: 20
+    });
+
+    this.anims.create({
+      key: 'right',
+      frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
+      frameRate: 10,
+      repeat: -1
+    });
+  }
+
+  update(): void{
+    if (this.cursors.left.isDown)
+    {
+      this.player.setVelocityX(-160);
+
+      this.player.anims.play('left', true);
+    }
+    else if (this.cursors.right.isDown)
+    {
+      this.player.setVelocityX(160);
+
+      this.player.anims.play('right', true);
+    }
+    else
+    {
+      this.player.setVelocityX(0);
+
+      this.player.anims.play('turn');
+    }
+
+    if (this.cursors.up.isDown && this.player.body.touching.down)
+    {
+      this.player.setVelocityY(-330);
+    }
   }
 }
